9c2c10bf54d19d137dfb50bce5d51e1ba693c503
Brian Sullivan <brian@civicevolution.org>
2014-01-17 10:46:34 -0800

    use clear text password for deploy user

--- ce2-custom/recipes/init-db.rb
+++ ce2-custom/recipes/init-db.rb
@@ -1,32 +1,27 @@
 log "Initialize the database"
 
-
 username = node[:deploy][:ce2_test][:database][:username]
 password = node[:deploy][:ce2_test][:database][:password]
 postgres_password = node['postgresql']['password']['postgres']
 db_name = node[:deploy][:ce2_test][:database][:database]
-statement = %{psql -U postgres -h localhost -c "SELECT * FROM pg_database"}
+statement = %{psql -U postgres -c "SELECT * FROM pg_database"}
 owner = username
 
-log "create the user: #{username}"
-
-=begin
 execute "create-db-user-#{username}" do
-  command %{psql -U postgres postgres -c \"CREATE USER #{username} with ENCRYPTED PASSWORD '#{password}' CREATEDB NOCREATEUSER\"}
+  #command %{psql -U postgres postgres -c \"CREATE USER #{username} with ENCRYPTED PASSWORD '#{password}' CREATEDB NOCREATEUSER\"}
+  command %{psql -U postgres postgres -c \"CREATE USER #{username} with PASSWORD '#{password}' CREATEDB NOCREATEUSER\"}
   not_if %{psql -U postgres -c "select * from pg_roles" | grep #{username}}
 end
 
-log "create the database"
 execute "create database for #{db_name}" do
-  command %{psql -U postgres postgres -c \"CREATE DATABASE #{db_name} OWNER #{owner}\"}
+  command %{psql -U postgres -c \"CREATE DATABASE #{db_name} OWNER #{owner}\"}
   not_if "#{statement} | grep #{db_name}"
 end
 
+
+# pg_last_xlog_receive_location() probably returns something once the db has been in use
 execute "alter-db-user-postgres" do
-  command %{psql -U postgres postgres -c \"ALTER USER postgres with ENCRYPTED PASSWORD '#{postgres_password}'\"}
+  command %{psql -U postgres -c \"ALTER USER postgres with ENCRYPTED PASSWORD '#{postgres_password}'\"}
   not_if %{psql -c "select pg_last_xlog_receive_location()" | grep "/"}
 end
 
-
-log "add the extension TBD"
-=end
