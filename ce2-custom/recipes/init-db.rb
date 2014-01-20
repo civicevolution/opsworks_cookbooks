dc21fbd7e9ca90da166dba70ee4c1391e9321c38
Brian Sullivan <brian@civicevolution.org>
2014-01-20 10:25:26 -0800

    Add recipe to run rake db:seed

--- ce2-custom/recipes/init-db.rb
+++ ce2-custom/recipes/init-db.rb
@@ -1,27 +1,34 @@
-log "Initialize the database"
+log "^^^^^^ custom recipe ce2-custom::init-db"
 
-username = node[:deploy][:ce2_ver_1][:database][:username]
-password = node[:deploy][:ce2_ver_1][:database][:password]
-postgres_password = node['postgresql']['password']['postgres']
-db_name = node[:deploy][:ce2_ver_1][:database][:database]
-statement = %{psql -U postgres -c "SELECT * FROM pg_database"}
-owner = username
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'rails'
 
-execute "create-db-user-#{username}" do
-  #command %{psql -U postgres postgres -c \"CREATE USER #{username} with ENCRYPTED PASSWORD '#{password}' CREATEDB NOCREATEUSER\"}
-  command %{psql -U postgres postgres -c \"CREATE USER #{username} with PASSWORD '#{password}' CREATEDB NOCREATEUSER\"}
-  not_if %{psql -U postgres -c "select * from pg_roles" | grep #{username}}
+    log "Initialize the rails database for #{application}"
+    username = node[:deploy][application][:database][:username]
+    password = node[:deploy][application][:database][:password]
+    postgres_password = node['postgresql']['password']['postgres']
+    db_name = node[:deploy][application][:database][:database]
+    statement = %{psql -U postgres -c "SELECT * FROM pg_database"}
+    owner = username
+
+    execute "create-db-user-#{username}" do
+      #command %{psql -U postgres postgres -c \"CREATE USER #{username} with ENCRYPTED PASSWORD '#{password}' CREATEDB NOCREATEUSER\"}
+      command %{psql -U postgres postgres -c \"CREATE USER #{username} with PASSWORD '#{password}' CREATEDB NOCREATEUSER\"}
+      not_if %{psql -U postgres -c "select * from pg_roles" | grep #{username}}
+    end
+
+    execute "create database for #{db_name}" do
+      command %{psql -U postgres -c \"CREATE DATABASE #{db_name} OWNER #{owner}\"}
+      not_if "#{statement} | grep #{db_name}"
+    end
+
+
+    # pg_last_xlog_receive_location() probably returns something once the db has been in use
+    execute "alter-db-user-postgres" do
+      command %{psql -U postgres -c \"ALTER USER postgres with ENCRYPTED PASSWORD '#{postgres_password}'\"}
+      not_if %{psql -c "select pg_last_xlog_receive_location()" | grep "/"}
+    end
+  end
 end
 
-execute "create database for #{db_name}" do
-  command %{psql -U postgres -c \"CREATE DATABASE #{db_name} OWNER #{owner}\"}
-  not_if "#{statement} | grep #{db_name}"
-end
-
-
-# pg_last_xlog_receive_location() probably returns something once the db has been in use
-execute "alter-db-user-postgres" do
-  command %{psql -U postgres -c \"ALTER USER postgres with ENCRYPTED PASSWORD '#{postgres_password}'\"}
-  not_if %{psql -c "select pg_last_xlog_receive_location()" | grep "/"}
-end
 
