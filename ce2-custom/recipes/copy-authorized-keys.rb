fa76af4879351ec887aaeeb626cec0564c58a65c
Brian Sullivan <brian@civicevolution.org>
2014-02-04 00:50:25 -0800

    recipe to copy authorized_keys to deploy

---
+++ ce2-custom/recipes/copy-authorized-keys.rb
@@ -0,0 +1,8 @@
+execute "Copy authorized_keys" do
+  command "cp /home/ec2-user/.ssh/authorized_keys /home/#{deploy[:user]}/.ssh/authorized_keys"
+  not_if { ::File.exists?("/home/#{deploy[:user]}/.ssh/authorized_keys") }
+end
+
+execute "Chown authorized_keys" do
+  command "chown #{deploy[:user]} /home/#{deploy[:user]}/.ssh/authorized_keys"
+end
