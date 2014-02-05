a7c1871aea40964bf699e41d0e9772279344ff8e
Brian Sullivan <brian@civicevolution.org>
2014-02-05 14:19:42 -0800

    Custom recipe to make the database.yml file

---
+++ ce2-custom/recipes/create-database-yml.rb
@@ -0,0 +1,23 @@
+log "^^^^^^ Feb 5 14:16 custom recipe ce2-custom::create-database-yml"
+
+node[:deploy].each do |application, deploy|
+
+  log "^^^^^^ Feb 5 14:16 application: #{application}, deploy[:application_type]: #{deploy[:application_type]}"
+
+  if deploy[:application_type] != 'rails'
+    log("Skipping ce2-custom::init-db for application #{application} as it is not a Rails app")
+    next
+  end
+
+  template "#{node[:deploy][application][:deploy_to]}/shared/config/database.yml.custom" do
+    cookbook "rails"
+    source "database.yml.erb"
+    mode "0660"
+    owner node[:deploy][application][:user]
+    group node[:deploy][application][:group]
+    variables(
+        :database => node[:deploy][application][:database],
+        :environment => node[:deploy][application][:rails_env]
+    )
+  end.run_action(:create)
+end
